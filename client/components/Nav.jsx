e8e1103db3b861520e0fd690caf03d50079da078
jessica draper <j.b.draper@hotmail.com>
2018-04-20 10:29:10 +1200

    Added placeholder banner to Nav

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -34,8 +34,9 @@ class Nav extends React.Component {
     return (
       <Router>
         <div>
+        <img className='headerImage' src="images/placeholder-banner.jpg" alt='logo'/>
           <nav className="navbar navbar-expand-md navbar-light bg-light navBar textColourNav">
-            <Link to='/' className="navbar-brand"><i className="fas fa-home faFaFont"></i></Link>
+            <Link to='/' className="navbar-brand"></Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
