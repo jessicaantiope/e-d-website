6cb88b2edb0f2a9e75055df006dd21eb112e5ee8
jessica draper <j.b.draper@hotmail.com>
2018-04-19 22:17:33 +1200

    Home.jsx setup on localhost

---
+++ client/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h4>Welcome Home</h4>
+      </div>
+    )
+  }
+}
+
+export default Home
