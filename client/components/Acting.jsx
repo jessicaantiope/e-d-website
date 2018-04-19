8f449292bc843b566ef5a2a1a72626d1ccaba364
jessica draper <j.b.draper@hotmail.com>
2018-04-19 22:17:54 +1200

    Acting.jsx setup on localhost

---
+++ client/components/Acting.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Acting extends React.Component {
+  render() {
+    return (
+      <div>
+        <h4>Welcome Acting</h4>
+      </div>
+    )
+  }
+}
+
+export default Acting
