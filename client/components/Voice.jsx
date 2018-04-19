21061046b2e588bb23b5074dae76f49f2ecbc481
jessica draper <j.b.draper@hotmail.com>
2018-04-19 22:18:51 +1200

    Voice.jsx setup on localhost

---
+++ client/components/Voice.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Voice extends React.Component {
+  render() {
+    return (
+      <div>
+        <h4>Welcome Voice</h4>
+      </div>
+    )
+  }
+}
+
+export default Voice
