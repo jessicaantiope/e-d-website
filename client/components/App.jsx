c6ec695affb1c01a7ceeef4bb9963bd649a6b6f9
jessica draper <j.b.draper@hotmail.com>
2018-04-19 22:19:25 +1200

    Added Nav to App.jsx and working on local host

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {HashRouter as Router, Route, Link} from 'react-router-dom'
 
+import Nav from './Nav'
+
 const App = () => {
   return (
       <Router>
           <div id="app">
               <div className="title">
-                  <h1>Hello World</h1>
+                  <Nav />
               </div>
               <br/>
               <footer className="footer">
