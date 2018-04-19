b5323a588cb91d13860cc6c3a284854ece92f827
jessica draper <j.b.draper@hotmail.com>
2018-04-20 10:28:00 +1200

    Added placeholder text to Home.jsx

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -4,7 +4,11 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        <h4>Welcome Home</h4>
+        <h4>Welcome Home</h4><br/>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a pharetra diam, ac condimentum massa. Etiam mauris felis, congue non bibendum eget, euismod ut odio. Sed in sapien varius, fermentum dui vel, interdum diam. Etiam ac sagittis lorem. Praesent eu scelerisque libero, nec malesuada erat. Nunc commodo ac mi vitae ultrices. Fusce ultricies ligula commodo iaculis fermentum. Nulla facilisi. Maecenas hendrerit quam eget placerat bibendum. Integer laoreet venenatis quam ut rhoncus. Suspendisse ac neque arcu. Vivamus vel luctus tortor. Donec scelerisque in tortor a imperdiet. In hac habitasse platea dictumst.</p><br/>
+
+        <p>Morbi eget turpis nisl. Fusce non tempus tellus, vulputate faucibus felis. Suspendisse purus eros, auctor fermentum dictum vitae, tempus non nisl. Phasellus ultrices aliquet sapien, id faucibus ante. Maecenas elementum bibendum dui ut semper. Pellentesque pretium, est vel lobortis cursus, augue libero varius metus, volutpat mattis massa felis non velit. Nulla ultricies risus sit amet justo imperdiet placerat. Morbi sed quam euismod, dictum ipsum sed, tempus ligula. Pellentesque neque arcu, pretium pretium varius vitae, molestie molestie arcu. Mauris tristique urna posuere facilisis imperdiet. Integer convallis sodales tellus nec viverra. Nunc dictum fringilla diam at euismod.
+        </p>
       </div>
     )
   }
